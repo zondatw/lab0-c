e4d3cc92e5086a8bb5c2735685e7d1560941582b
zonda yang <u226699@gmail.com>
2022-03-08 22:52:43 +0800

    Add q_insert_head of queue.c

--- queue.c
+++ queue.c
@@ -42,6 +42,23 @@ void q_free(struct list_head *l)
  */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+
+    element_t *node = malloc(sizeof(element_t));
+    if (!node) {
+        return false;
+    }
+
+    node->value = strdup(s);
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+
+    list_add(&node->list, head);
+
     return true;
 }
 
