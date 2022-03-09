d94804bdbdb3a0cf0ae88529c33f53af4ce9cda5
zonda yang <u226699@gmail.com>
2022-03-09 21:01:03 +0800

    Add q_remove_head of queue.c

--- queue.c
+++ queue.c
@@ -115,7 +115,22 @@ bool q_insert_tail(struct list_head *head, char *s)
  */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *node = list_first_entry(head, element_t, list);
+    if (!node) {
+        return NULL;
+    }
+
+    list_del(head->next);
+
+    if (sp) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return node;
 }
 
 /* Attempt to remove element from tail of queue.
